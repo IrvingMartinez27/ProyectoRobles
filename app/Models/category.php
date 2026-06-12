be507c311d45b2ddcb357fc488ae120b8e09a44e
alex20mx <alexis.tesch2023@gmail.com>
2026-06-12 00:56:38 -0600

    Bug solucionado

--- app/Models/category.php
+++ app/Models/category.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Traits\BelongsToTenant;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Str;
 
 class category extends Model
 {
@@ -12,7 +13,31 @@ class category extends Model
 
     protected $table = 'categories';
 
-    protected $fillable = ['tenant_id','name'];
+    protected $fillable = ['tenant_id', 'name', 'slug'];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($category) {
+            if (empty($category->slug)) {
+                $baseSlug = Str::slug($category->name);
+                $slug = $baseSlug;
+                $counter = 1;
+
+                while (
+                    static::where('tenant_id', $category->tenant_id)
+                        ->where('slug', $slug)
+                        ->exists()
+                ) {
+                    $slug = $baseSlug . '-' . $counter;
+                    $counter++;
+                }
+
+                $category->slug = $slug;
+            }
+        });
+    }
 
     public function products()
     {
